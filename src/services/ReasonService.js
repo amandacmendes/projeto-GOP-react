a8277af2c81be162509ee10ff0458f4e9352f5f2
Amanda Mendes <83428587+amandacmendes@users.noreply.github.com>
2023-09-17 17:01:35 -0400

    updated reason service

--- src/services/ReasonService.js
+++ src/services/ReasonService.js
@@ -3,7 +3,7 @@ import { api } from "./api";
 class ReasonService {
 
     async getReasonTypes() {
-        const result = await api.get('/officer', {
+        const result = await api.get('/reasontype', {
             headers: {
                 Authorization: 'Bearer ' + sessionStorage.getItem('token'),
                 'Content-Type': 'application/json',
@@ -13,9 +13,9 @@ class ReasonService {
         return result;
     }
 
-    async getReasonfromOperation() {
+    async getReasonfromOperation(id) {
 
-        const officers = await api.get('/officer', {
+        const reasons = await api.get(`/reason/operation/${id}`, {
             headers: {
                 Authorization: 'Bearer ' + sessionStorage.getItem('token'),
                 'Content-Type': 'application/json',
@@ -23,43 +23,37 @@ class ReasonService {
             }
         });
 
-        const teams = await api.get('/team', {
+        return reasons;
+    }
+
+    async createReason(data){
+        const result = await api.post('/reason', {
+            description: data.description,
+            reasontype_id: data.reasontype_id,
+            operation_id: data.operation_id
+        }, {
             headers: {
                 Authorization: 'Bearer ' + sessionStorage.getItem('token'),
                 'Content-Type': 'application/json',
                 Accept: "*/*"
             }
         });
+        return result;
+    }
 
-        // Create a mapping of teams by their id
-        const teamMap = new Map();
-
-        teams.data.forEach(team => {
-            teamMap.set(team.id, team);
-        });
-
-        // Create a new array of officers with the nested team information
-        const officersWithTeams = officers.data.map(officer => {
-            const team = teamMap.get(officer.team_id);
-            if (team) {
-                // If a matching team is found, create a new object with the nested structure
-                return {
-                    ...officer,
-                    team: {
-                        id: team.id,
-                        team_name: team.team_name,
-                        status: team.status,
-                        createdAt: team.createdAt,
-                        updatedAt: team.updatedAt
-                    }
-                };
-            } else {
-                // If no matching team is found, simply return the officer object
-                return officer;
+    async updateReason(data){
+        const result = await api.put('/reason', {
+            description: data.description,
+            reasontype_id: data.reasontype_id,
+            operation_id: data.operation_id
+        }, {
+            headers: {
+                Authorization: 'Bearer ' + sessionStorage.getItem('token'),
+                'Content-Type': 'application/json',
+                Accept: "*/*"
             }
         });
-
-        return officersWithTeams;
+        return result;
     }
 
 
