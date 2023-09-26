2cf2755bfd5363758662be222ba0ea61d75b9f90
Amanda Mendes <83428587+amandacmendes@users.noreply.github.com>
2023-09-25 20:49:04 -0400

    main page done

--- src/pages/Index/Mainpage.js
+++ src/pages/Index/Mainpage.js
@@ -1,14 +1,56 @@
 import '../../css/style.css';
-import { LeftMenu } from '../../components/LeftMenu';
+import '../../css/card_mainpage.css';
+
 import { ContentBase } from '../../components/ContentBase';
+import dashboard_img from '../../img/dashboard-edit-2.jpg';
+import operacao_img from '../../img/policial-1.png';
+import { Card, Col, Image, Row } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
 
 export function MainPage() {
     return (
         <>
-        <ContentBase/>
-        <div className='container'>
-            <h1>Página Principal</h1>
-        </div>
+            <ContentBase />
+            <div className='container'>
+                <h1>Página Principal</h1>
+
+                <Row className='mt-5'>
+                    <Col>
+                        <Link to="/operation">
+                            <Card style={{ height: "250px" }} >
+                                <Card.Img src={operacao_img} alt="operation-image" className="mb-3 h-100" />
+                                <div className="card-overlay"></div>
+                                <Card.ImgOverlay>
+                                    <Card.Title className='text-light h-100 d-flex align-items-center justify-content-center'
+                                    >
+                                        <h2>
+                                            Operações Policiais
+                                        </h2>
+                                    </Card.Title>
+                                </Card.ImgOverlay>
+                            </Card>
+                        </Link>
+
+                    </Col>
+                    <Col>
+                        <Link to="/dashboard">
+                            <Card style={{ height: "250px" }}>
+                                <Card.Img src={dashboard_img} alt="dashboard-image" className="img-fluid mb-3 h-100" />
+                                <div className="card-overlay "></div>
+                                <Card.ImgOverlay>
+                                    <Card.Title className='text-light h-100 d-flex align-items-center justify-content-center'>
+                                        <h2>
+                                            Dashboard
+                                        </h2>
+                                    </Card.Title>
+                                </Card.ImgOverlay>
+                            </Card>
+                        </Link>
+                    </Col>
+                </Row>
+
+            </div>
         </>
     );
 }
