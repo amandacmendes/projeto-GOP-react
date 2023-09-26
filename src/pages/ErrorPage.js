eaa8dda9f383b89873a04bad243fc3082b1c8255
Amanda Mendes <83428587+amandacmendes@users.noreply.github.com>
2023-09-25 22:55:44 -0400

    minor updates on error page

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,12 +1,19 @@
 import '../css/style.css';
 import { ContentBase } from '../components/ContentBase';
+import error_img from '../img/404-error.png'
+import { Image } from 'react-bootstrap';
 
 export function ErrorPage() {
     return (
         <>
-            <ContentBase />
+            {sessionStorage.getItem('id') &&
+                <ContentBase />
+            }
             <div className='container'>
-                <h1>Ops! Parece que a página que você tentou acessar não existe.</h1>
+                <h1 className='py-4 text-center'>Ops! Parece que a página que você tentou acessar não existe.</h1>
+                <div className='d-flex h-100 align-content-middle justify-content-center'>
+                    <Image src={error_img} className='img-fluid' style={{ maxWidth: '40%', maxHeight: '40%' }} />
+                </div>
             </div>
         </>
     );
