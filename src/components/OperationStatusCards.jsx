7e4d221a901d8a144d1bcd80e9fbcc5094718058
Amanda Mendes <83428587+amandacmendes@users.noreply.github.com>
2023-09-27 10:23:26 -0400

    minor updates card design / statusTag

--- src/components/OperationStatusCards.jsx
+++ src/components/OperationStatusCards.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card } from 'react-bootstrap';
+import { Card, Col, Row } from 'react-bootstrap';
 
 function OperationStatusCards({ operations }) {
     // Calculate the number of operations in each status
@@ -7,6 +7,7 @@ function OperationStatusCards({ operations }) {
         OPENED: 0,
         TRIGGERED: 0,
         FINISHED: 0,
+        CANCELED: 0
         // Add more status counts as needed
     };
 
@@ -19,36 +20,40 @@ function OperationStatusCards({ operations }) {
 
     return (
         <div className="operation-status-cards d-flex flex-row justify-content-between">
-            <Card>
-                <Card.Body>
-                    <h4 className='text-primary' >Operações Abertas</h4>
-                    <h4 style={{ fontWeight: 'normal' }}>{statusCounts.OPENED}</h4>
-                </Card.Body>
-            </Card>
-            <Card>
-                <Card.Body>
-                    <h4 style={{ color: 'orange' }} >Operações Deflagradas</h4>
-                    <h4 style={{ fontWeight: 'normal' }}>{statusCounts.TRIGGERED}</h4>
-                </Card.Body>
-            </Card>
-            <Card>
-                <Card.Body>
-                    <h4 className='text-danger' >Operações Finalizadas</h4>
-                    <h4 style={{ fontWeight: 'normal' }}>{statusCounts.FINISHED}</h4>
-                </Card.Body>
-            </Card>
-            {/* 
-            <div className="status-card">
-            </div>
-            <div className="status-card">
-                <h3>Triggered Operations</h3>
-                <p>{statusCounts.TRIGGERED}</p>
-            </div>
-            <div className="status-card">
-                <h3>Finished Operations</h3>
-                <p>{statusCounts.FINISHED}</p>
-            </div>
-            Add more status cards as needed */}
+            <Row>
+                <Col className='col-sm-12 col-xs-12 col-md-3'>
+                    <Card>
+                        <Card.Body>
+                            <h4 className='text-primary' >Operações Abertas</h4>
+                            <h4 style={{ fontWeight: 'normal' }}>{statusCounts.OPENED}</h4>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col className='col-sm-12 col-xs-12 col-md-3'>
+                    <Card>
+                        <Card.Body>
+                            <h4 style={{ color: 'orange' }} >Operações Deflagradas</h4>
+                            <h4 style={{ fontWeight: 'normal' }}>{statusCounts.TRIGGERED}</h4>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col className='col-sm-12 col-xs-12 col-md-3'>
+                    <Card>
+                        <Card.Body>
+                            <h4 className='text-success' >Operações Finalizadas</h4>
+                            <h4 style={{ fontWeight: 'normal' }}>{statusCounts.FINISHED}</h4>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col className='col-sm-12 col-xs-12 col-md-3'>
+                    <Card>
+                        <Card.Body>
+                            <h4 className='text-danger' >Operações Canceladas</h4>
+                            <h4 style={{ fontWeight: 'normal' }}>{statusCounts.CANCELED}</h4>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
         </div>
     );
 }
